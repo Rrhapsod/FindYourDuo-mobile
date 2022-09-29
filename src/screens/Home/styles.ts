c8f875eae73e5d5a60a614ad779d58cc1fde3964
Renan Rondon <renanrondon@live.com>
2022-09-28 23:49:28 -0300

    Game screen done

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -8,8 +8,8 @@ export const styles = StyleSheet.create({
   logo: {
     width: 214,
     height: 120,
-    marginTop: 74,
-    marginBottom: 48,
+    marginTop: 54,
+    marginBottom: 38,
   },
   contentList: {
     paddingLeft: 32,
